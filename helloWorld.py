96b4e2814c17530af9309b5a998142fc149a6bc5
Hector Neira <handneira14@gmail.com>
2024-11-06 20:40:29 -0500

    Nuevo Codigo

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,13 @@
-prin("Hola Mundo")
+class Persona():
+    def __init__(self, nombre, edad, profesion) -> None:
+        self.nombre = nombre
+        self.edad = edad
+        self.profesion = profesion
+    def saludar(self):
+        print("Hola")
+    def despedirse(self):
+        print("Chao")
+
+persona_1 = Persona("Hector", 36, "IngenieroSistemas")
+print(persona_1.nombre)
+persona_1.saludar()
